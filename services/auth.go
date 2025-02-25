a42fa7ebebad4dd33a2de9b8b47fbf83c5aa4b58
Reihan Agam <reihanagam7@gmail.com>
2025-02-25 11:15:08 +0700

    add send email

--- services/auth.go
+++ services/auth.go
@@ -223,6 +223,20 @@ func Register(u *models.User) (map[string]interface{}, error) {
 		return nil, errors.New(errInsertProfile.Error())
 	}
 
+	emailBody := `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+	<div style="margin:50px auto; width:70%; padding: 20px 0;">
+	<p style="font-size:1.1em;">Hi,</p>
+	<p>Use the following OTP to complete your Sign Up procedures. OTP is valid for 2 minutes</p>
+	<h2 style="background: #00466a; margin: 0 auto; width: max-content; padding: 0 10px; color: #fff; border-radius: 4px;">` + otp + `</h2>
+	<p style="font-size:0.9em;">Regards, <br/>TJL</p>
+	<hr style="border:none;border-top:1px solid #eee" />
+	</div></div>`
+	
+    errEmail := helper.SendEmail(user.Email, "TJL", emailBody)
+    if errEmail != nil {
+        helper.Logger("error", "Failed to send email: "+errEmail.Error())
+    }
+
 	token, err := middleware.CreateToken(user.Id)
 	if err != nil {
 		helper.Logger("error", "In Server: "+err.Error())
