815d4eb81b3d9f41e8f85183024aa5de9d6ea7d8
Reihan Agam <reihanagam7@gmail.com>
2025-02-26 10:35:42 +0700

    update

--- models/user.go
+++ models/user.go
@@ -6,6 +6,7 @@ import (
 
 type User entities.User
 type Profile entities.Profile
+type ProfileResponse entities.ProfileResponse
 type CheckAccount entities.CheckAccount
 type UserLogin entities.UserLogin
 type UserOtp entities.UserOtp
