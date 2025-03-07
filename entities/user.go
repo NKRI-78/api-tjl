0118d1fc4c3d52f807e548ee95087c1db3991aef
Reihan Agam <reihanagam7@gmail.com>
2025-03-07 17:04:42 +0700

    update

--- entities/user.go
+++ entities/user.go
@@ -20,24 +20,42 @@ type User struct {
 }
 
 type Profile struct {
-	Id       string `json:"id"`
-	Avatar   string `json:"avatar"`
-	Phone    string `json:"phone"`
-	Fullname string `json:"fullname"`
-	Enabled  int    `json:"enabled"`
-	Email    string `json:"email"`
-	JobId    string `json:"job_id"`
-	JobName  string `json:"job_name"`
+	Id           string `json:"id"`
+	Avatar       string `json:"avatar"`
+	Phone        string `json:"phone"`
+	Fullname     string `json:"fullname"`
+	Enabled      int    `json:"enabled"`
+	Email        string `json:"email"`
+	JobId        string `json:"job_id"`
+	JobName      string `json:"job_name"`
+	BioBirthdate string `json:"bio_birthdate"`
+	BioGender    string `json:"bio_gender"`
+	BioHeight    string `json:"bio_height"`
+	BioWeight    string `json:"bio_weight"`
+	BioStatus    string `json:"bio_status"`
+	BioReligion  string `json:"bio_religion"`
+	BioPlace     string `json:"bio_place"`
 }
 
 type ProfileResponse struct {
-	Id        string             `json:"id"`
-	Avatar    string             `json:"avatar"`
-	Phone     string             `json:"phone"`
-	Fullname  string             `json:"fullname"`
-	IsEnabled bool               `json:"enabled"`
-	Email     string             `json:"email"`
-	Job       ProfileJobResponse `json:"job"`
+	Id          string             `json:"id"`
+	Avatar      string             `json:"avatar"`
+	Phone       string             `json:"phone"`
+	Fullname    string             `json:"fullname"`
+	IsEnabled   bool               `json:"enabled"`
+	Email       string             `json:"email"`
+	Job         ProfileJobResponse `json:"job"`
+	FormBiodata ProfileFormBiodata `json:"form_biodata"`
+}
+
+type ProfileFormBiodata struct {
+	Birthdate string `json:"birthdate"`
+	Gender    string `json:"gender"`
+	Height    string `json:"height"`
+	Weight    string `json:"weight"`
+	Religion  string `json:"religion"`
+	Place     string `json:"place"`
+	Status    string `json:"status"`
 }
 
 type ProfileJobResponse struct {
