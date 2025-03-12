eaeaee430f4529ee2b9b5ff99395c1e1f278237b
Reihan Agam <reihanagam7@gmail.com>
2025-03-12 12:38:20 +0700

    update

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 	router := mux.NewRouter()
 
 	router.Use(middleware.JwtAuthentication)
-	router.Use(middleware.RateLimitingMiddleware)
+	// router.Use(middleware.RateLimitingMiddleware)
 
 	// Check if the directory exists, create if it doesn't
 	errMkidr := os.MkdirAll("public", os.ModePerm) // os.ModePerm ensures directory is created with the correct permissions
