2bdfedbc22c2a57b95e01e430ef7f111b0705c68
Reihan Agam <reihanagam7@gmail.com>
2025-04-14 09:56:20 +0700

    update

--- controllers/init_fcm.go
+++ controllers/init_fcm.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 	helper "superapps/helpers"
 	"superapps/models"
@@ -12,6 +13,14 @@ import (
 func InitFcm(w http.ResponseWriter, r *http.Request) {
 
 	data := &models.InitFcm{}
+
+	err := json.NewDecoder(r.Body).Decode(data)
+
+	if err != nil {
+		helper.Logger("error", "In Server: "+err.Error())
+		helper.Response(w, 400, true, "Internal server error ("+err.Error()+")", map[string]any{})
+		return
+	}
 
 	tokenHeader := r.Header.Get("Authorization")
 
