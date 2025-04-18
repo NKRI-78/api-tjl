8813c5dd4cab05c83a547e45c34757ee8e49f6a6
Reihan Agam <reihanagam7@gmail.com>
2025-04-18 11:14:08 +0700

    update

--- controllers/forum_store.go
+++ controllers/forum_store.go
@@ -54,5 +54,5 @@ func ForumStore(w http.ResponseWriter, r *http.Request) {
 	}
 
 	helper.Logger("info", "Forum Store success")
-	helper.Response(w, http.StatusOK, false, "Successfully", result["id"])
+	helper.Response(w, http.StatusOK, false, "Successfully", result["data"])
 }
