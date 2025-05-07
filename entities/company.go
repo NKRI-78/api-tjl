f82c85f246a0bc45ce3c5d75a8b00a4ab0c43205
Reihan Agam <reihanagam7@gmail.com>
2025-05-07 11:31:32 +0700

    update

--- entities/company.go
+++ entities/company.go
@@ -1,5 +1,7 @@
 package entities
 
+import "time"
+
 type CompanyStore struct {
 	Id      int    `json:"id"`
 	Logo    string `json:"logo"`
@@ -19,15 +21,17 @@ type CompanyUpdate struct {
 }
 
 type CompanyListQuery struct {
-	Id          string `json:"id"`
-	Logo        string `json:"logo"`
-	Name        string `json:"name"`
-	CompanyName string `json:"company_name"`
+	Id          string    `json:"id"`
+	Logo        string    `json:"logo"`
+	Name        string    `json:"name"`
+	CreatedAt   time.Time `json:"created_at"`
+	CompanyName string    `json:"company_name"`
 }
 
 type CompanyList struct {
-	Id     string `json:"id"`
-	Logo   string `json:"logo"`
-	Name   string `json:"name"`
-	Origin string `json:"origin"`
+	Id        string    `json:"id"`
+	Logo      string    `json:"logo"`
+	Name      string    `json:"name"`
+	Origin    string    `json:"origin"`
+	CreatedAt time.Time `json:"created_at"`
 }
