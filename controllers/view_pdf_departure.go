7d4f2c21090a44b5c67a6b1b07cda2faf57eb368
Reihan Agam <reihanagam7@gmail.com>
2025-05-08 14:29:56 +0700

    update

---
+++ controllers/view_pdf_departure.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"net/http"
+	helper "superapps/helpers"
+	"superapps/services"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func ViewPdfDeparture(w http.ResponseWriter, r *http.Request) {
+
+	tokenHeader := r.Header.Get("Authorization")
+
+	token := helper.DecodeJwt(tokenHeader)
+
+	claims, _ := token.Claims.(jwt.MapClaims)
+
+	userId, _ := claims["id"].(string)
+
+	result, err := services.ViewPdfDeparture(userId)
+
+	if err != nil {
+		helper.Response(w, 400, true, err.Error(), map[string]any{})
+		return
+	}
+
+	helper.Logger("info", "View PDF Departure success")
+	helper.Response(w, http.StatusOK, false, "Successfully", result["data"])
+}
